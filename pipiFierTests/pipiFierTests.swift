78bf303ee55cd65c018b31210937b4749e81be3d
Enver Francisco <enver.francisco@gmail.com>
2025-04-21 03:55:20 -0400

    Initial Commit

---
+++ pipiFierTests/pipiFierTests.swift
@@ -0,0 +1,17 @@
+//
+//  pipiFierTests.swift
+//  pipiFierTests
+//
+//  Created by Enver Daniel Francisco Báez on 4/21/25.
+//
+
+import Testing
+@testable import pipiFier
+
+struct pipiFierTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
